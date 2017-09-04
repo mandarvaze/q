664cbc7dc342ee768f3939f00b7af5db881c2049
Mandar Vaze <mandarvaze@gmail.com>
2017-09-04 11:26:55 +0530

    Bumped up version to 0.1.2 (inspect and pretty print)

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Q.Mixfile do
 
   def project do
     [app: :qq,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
